e55ea108f286ae9fd645e9816a4da5e70c9891bb
Jack Zhu <jackzhu426@gmail.com>
2024-07-14 12:04:20 +1000

    feat: add all unit tests for schedule controller

---
+++ src/schedule/__tests__/controller/findOne.test.ts
@@ -0,0 +1,27 @@
+import { TestBed } from "@automock/jest";
+
+import { ScheduleService } from "src/schedule/schedule.service";
+import { ScheduleController } from "src/schedule/schedule.controller";
+
+describe("ScheduleController/findOne", () => {
+  let scheduleController: ScheduleController;
+  let scheduleService: jest.Mocked<ScheduleService>;
+
+  beforeEach(() => {
+    const { unit, unitRef: container } = TestBed.create(ScheduleController).compile();
+    scheduleController = unit;
+    scheduleService = container.get(ScheduleService);
+  });
+
+  it("should invoke the schedule service findOne function", async () => {
+    jest.spyOn(scheduleService, "validateCreateSchedule").mockImplementation();
+
+    const mockFindOne = jest.spyOn(scheduleService, "findOne").mockResolvedValue({} as any);
+
+    const res = await scheduleController.findOne("123");
+
+    expect(mockFindOne).toHaveBeenCalledWith("123");
+
+    expect(res).toEqual({});
+  });
+});
